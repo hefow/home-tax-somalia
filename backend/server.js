0d4e1c8d473c729d32ee8138d2f3b1a9d76f197a
alidiamond1 <calilucky3@gmail.com>
2024-10-20 06:37:53 +0300

    waxaa kuxeray database mongodb compass iyo port aan isticmaleeno database

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from "express";
+import connectDB from './config/database.js';
 
 
 const app = express();
+
+
 
 app.use(express.json());
 
@@ -9,8 +12,11 @@ app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.listen(process.env.PORT, () => {
+    // Connect to database
+    connectDB();
+    console.log(`Server is running on port ${process.env.PORT}`);
 });
+
 
 
